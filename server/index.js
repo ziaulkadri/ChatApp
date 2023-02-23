e889167b70995aaa473c7cf45fac2ac9bb626b56
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-23 12:19:04 +0530

    backend:setup the socket.io backend server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http');
+
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
+	},
+});
+
+io.on('connection', (socket) => {
+	console.log(socket.id);
+
+	socket.on('disconnect', () => {
+		console.log('user diconnected', socket.id);
+	});
+});
+
+server.listen(3001, () => {
+	console.log('server running on port 3001');
+});
